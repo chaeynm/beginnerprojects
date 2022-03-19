7494be7e59be6b22ce217d67e87fef1a3153fb67
chaeynm <73467285+chaeynm@users.noreply.github.com>
2022-03-19 10:16:02 -0400

    edits to number guessing game

--- guessnum.py
+++ guessnum.py
@@ -14,5 +14,26 @@ def guess(x):
             print("pls type in a number")
 
     print(f'yay! congrats u have guessed the number {random_number}')
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low!= high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high bc low = high
+
+        feedback = input(f'is guess {guess} too high (H), too low (L), or correct (C)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'yay! the computer guessed your number, {guess}, correctly!')
+
+computer_guess(9999)
+
+
 
