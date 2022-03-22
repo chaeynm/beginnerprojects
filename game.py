9d88af08a34408886ddb4d7c8b2ea4833a123d21
chaeynm <73467285+chaeynm@users.noreply.github.com>
2022-03-22 13:22:44 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,75 @@
+#author credits to kylie ying
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]    # using a single list to represent 3x3 board
+        self.current_winner = None      # keeping track of winner
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| '+' | '.join(row)+' |')
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]for j in range(3)]
+        for row in number_board:
+            print('| '+' | '.join(row)+' |')
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def make_move(self, square,letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square,letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    # moves = []
+    # for (i,spot) in enumerate(self.board):
+    #     if spot == ' ':
+    #         moves.append(i)
+    #     return moves
+
+    def winner(self, square,letter):
+        row_ind = square // 3
+        row = self.board[row_ind*3 : ]
+
+
+    def play(game, x_player, o_player, print_game = True):
+        if print_game:
+            game.print_board_nums()
+        letter = 'X'
+
+        while game.empty_squares():
+            if letter == 'O':
+                square = o_player.get_move(game)
+            else:
+                square = o_player.get_move(game)
+
+            if game.make_move(square,letter):
+                if print_game:
+                    print(letter + f' makes a move to square {square}')
+                    game.print_board()
+                    print('')
+
+            if game.current_winner:
+                if print_game:
+                    print(letter+ ' wins!')
+                return letter
+
+                letter = 'O' if letter == 'X' else 'X'
+                # if letter == 'x':
+                #    letter == 'o'
+                # else:
+                #    letter == 'x'
+
+            if print_game:
+                print('It\'s a tie')
