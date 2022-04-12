5fb6945708d1b65156675ca4851e55ed983d2232
Chaeyoun Myoung <73467285+chaeynm@users.noreply.github.com>
2022-04-12 00:17:06 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -39,9 +39,31 @@ class TicTacToe:
     #     return moves
 
     def winner(self, square,letter):
+        # winner if 3 in a row anywhere, we have to check all of these
+        # checking the rows
         row_ind = square // 3
-        row = self.board[row_ind*3 : ]
+        row = self.board[row_ind*3 : (row_ind+1)*3]
+        if all([spot == letter for spot in row]):
+            return True
+        # check columns
+        col_ind = square % 3
+        column = [self.board[col_ind+3] for i in range(3)]
+        if all([spot == letter for spot in row]):
+            return True
 
+    # check diagonals
+    # but only if the square is an even number
+    # these are the only moves possible to win a diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]] # left to right diagonal
+                if all([spot == letter for spot in diagonal1]):
+                    return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]] # right to left diagonal
+                if all([spot == letter for spot in diagonal2]):
+                    return True
+
+        #if all these fail return false
+            return False
 
     def play(game, x_player, o_player, print_game = True):
         if print_game:
@@ -65,7 +87,7 @@ class TicTacToe:
                     print(letter+ ' wins!')
                 return letter
 
-                letter = 'O' if letter == 'X' else 'X'
+            letter = 'O' if letter == 'X' else 'X'
                 # if letter == 'x':
                 #    letter == 'o'
                 # else:
@@ -73,3 +95,6 @@ class TicTacToe:
 
             if print_game:
                 print('It\'s a tie')
+
+    if __name__ == '__main__':
+        x_player = HumanPlayer('X')
